6fc2ce8ce5704298b3a0966bd7b950f295c91e2c
ishanp <eshanpurekar@gmail.com>
2025-01-01 18:57:45 +0900

    Remove @st.cache_data on maiden_overs_by_season AND all_hattricks

--- database.py
+++ database.py
@@ -887,7 +887,7 @@ class DB:
 
         return s[0]
 
-    @st.cache_data
+    #@st.cache_data
     def all_hattricks(_self):
         _self.my_cursor.execute('''
         CALL hatTrick_players_by_season()
